f8d7606a8de323bb55ff2056295d85ff1aa3bf78
SoffySchmidt <sofschmi@gmail.com>
2018-09-30 18:47:00 +0200

    Soffy Sprite Test 2

---
+++ TileMapTest/Assets/MoveAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnim : MonoBehaviour {
+
+    Vector3 tempPos;
+    Animator anim;
+    AnimatorClipInfo[] animInfo; 
+
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent(Animation).animation.Play("New Sprite 1");
+        anim.SetTrigger("New Sprite 1");
+
+        
+ 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempPos = transform.position;
+        
+
+    
+
+        tempPos.x -= 0.001f;
+
+        transform.position = tempPos;
+	}
+}
